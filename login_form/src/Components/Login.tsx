8341ce45a56f8432db26b15a50d36e0c573f82b8
Alex Cheung <alexc.sfu@gmail.com>
2022-07-26 23:41:56 -0700

    added username and password field to the form

--- login_form/src/Components/Login.tsx
+++ login_form/src/Components/Login.tsx
@@ -17,7 +17,39 @@ const Login = () => {
     setErrMsg("");
   }, [user, pwd]);
 
-  return <div></div>;
+  return (
+    <section>
+      <p
+        ref={errRef}
+        className={errMsg ? "errmsg" : "offscreen"}
+        aria-live="assertive"
+      >
+        {errMsg}
+      </p>
+      <h1>Sign In</h1>
+      <form>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          ref={userRef}
+          autoComplete="off"
+          onChange={(e) => setUser(e.target.value)}
+          value={user}
+          required
+        />
+        <label htmlFor="password">Passowrd:</label>
+        <input
+          type="password"
+          id="password"
+          onChange={(e) => setPwd(e.target.value)}
+          value={pwd}
+          required
+        />
+        <button>Sign In</button>
+      </form>
+    </section>
+  );
 };
 
 export default Login;
